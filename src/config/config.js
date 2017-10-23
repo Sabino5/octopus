0161f0fb1189c6df943ed41510640c64ecef0d93
Sabino5 <33041511+Sabino5@users.noreply.github.com>
2017-10-23 18:01:46 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,9 @@
 export default {
-<script src="https://www.gstatic.com/firebasejs/4.6.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
+  authProvider: "google",
     apiKey: "AIzaSyADKl40I2XH_pBMKYUWK9nHRjS2svbIrqQ",
     authDomain: "wikitest-63fdf.firebaseapp.com",
     databaseURL: "https://wikitest-63fdf.firebaseio.com",
     projectId: "wikitest-63fdf",
     storageBucket: "wikitest-63fdf.appspot.com",
-    messagingSenderId: "239418062752"
-  };
-  firebase.initializeApp(config);
-</script>
+    messagingSenderId: "239418062752
 };
